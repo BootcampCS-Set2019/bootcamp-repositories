652657ecc26620f02c074a5dff8b4ba9a477c587
Matheus Bispo <matheus.filipe.bispo@accenture.com>
2019-10-01 19:30:10 -0300

    Add initial project files

---
+++ Repositories/Repositories.h
@@ -0,0 +1,19 @@
+//
+//  Repositories.h
+//  Repositories
+//
+//  Created by matheus.filipe.bispo on 01/10/19.
+//  Copyright © 2019 BootcampCS-Set2019. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Repositories.
+FOUNDATION_EXPORT double RepositoriesVersionNumber;
+
+//! Project version string for Repositories.
+FOUNDATION_EXPORT const unsigned char RepositoriesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Repositories/PublicHeader.h>
+
+
