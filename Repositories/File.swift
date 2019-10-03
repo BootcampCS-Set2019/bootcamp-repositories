5691934addacab315618cd73e8fce04a1c059b92
Matheus Bispo <matheus.filipe.bispo@accenture.com>
2019-10-03 16:04:55 -0300

    Update initial project files

---
+++ Repositories/File.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  Repositories
+//
+//  Created by matheus.filipe.bispo on 03/10/19.
+//  Copyright © 2019 BootcampCS-Set2019. All rights reserved.
+//
+
+import Foundation
+import MTGSDKSwift
+
+class Teste {
+    init() {
+        var _ = Magic()
+    }
+}
--- RepositoriesTests/RepositoriesTests.swift
+++ RepositoriesTests/RepositoriesTests.swift
@@ -6,29 +6,17 @@
 //  Copyright © 2019 BootcampCS-Set2019. All rights reserved.
 //
 
-import XCTest
+import Quick
+import Nimble
 @testable import Repositories
 
-class RepositoriesTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class RepositoriesTests: QuickSpec {
+    override func spec() {
+        context("Teste") {
+            it("Será que funciona?") {
+                let teste = Teste()
+                expect(teste).toNot(beNil())
+            }
         }
     }
-
 }
